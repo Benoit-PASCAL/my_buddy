3a261d5a255aaf61960bcf905906b17ed73c1322
Benoît PASCAL <pcl.ben.6@gmail.com>
2024-03-19 11:10:38 +0100

    fix: apply voter when correct

--- src/Security/Voter/Chore/MainVoter.php
+++ src/Security/Voter/Chore/MainVoter.php
@@ -2,27 +2,36 @@
 
 namespace App\Security\Voter\Chore;
 
-use App\Entity\Chore\Assignment;
-use App\Entity\Chore\Permission;
-use App\Entity\Chore\User;
+use App\Chore\Entity\Assignment;
+use App\Chore\Entity\Permission;
+use App\Chore\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class MainVoter extends Voter
 {
-    protected function supports(string $attribute, mixed $subject): bool
+    public function supports(string $attribute, mixed $subject): bool
     {
-        if (!str_contains($attribute, '_')) {
+        // only vote on `ACCESS_` attributes
+        if (!str_starts_with($attribute, 'ACCESS_')) {
             return false;
         }
 
-        $controller = strtolower(explode('_', $attribute)[0]);
-        $action = explode('_', $attribute)[1] - 0;
-
-        if(!is_int($action)) {
+        // control if the attribute is well formatted => ACCESS_controller_action
+        $access = explode('_', $attribute);
+        if(count($access) !== 3) {
             return false;
         }
 
+        $controller = strtolower($access[1]);
+        $action = $access[2];
+
+        // control if the action is well formatted => integer
+        if(!ctype_digit($action)) {
+            return false;
+        }
+
+        // control if the controller is well formatted => in controllers list
         if(!in_array($controller, array_map(function($item) {
             return strtolower($item); } ,Permission::CONTROLLER_LIST))) {
             return false;
@@ -31,22 +40,25 @@ class MainVoter extends Voter
         return true;
     }
 
-    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    public function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
     {
-        $controller = strtolower(explode('_', $attribute)[0]);
-        $action = explode('_', $attribute)[1] - 0;
+        $controller = strtolower(explode('_', $attribute)[1]);
+        $action = intval(explode('_', $attribute)[2]);
 
         $user = $token->getUser();
-
 
         /** @var $user User */
         foreach ($user->getAssignments() as $assignment) {
             /** @var $assignment Assignment */
-            foreach ($assignment->getRole()->getPermissions() as $permission) {
-                /** @var $permission Permission */
-                if (strtolower($permission->getController()->getLabel()) === $controller) {
-                    if ($permission->getAccess() >= $action) {
-                        return true;
+            if($assignment->getStartDate() < new \DateTime() &&
+                ($assignment->getEndDate() === null || $assignment->getEndDate() > new \DateTime())) {
+                if($assignment->getRole()) {
+                    foreach ($assignment->getRole()->getPermissions() as $permission) {
+                        /** @var $permission Permission */
+                        if (strtolower($permission->getController()->getLabel()) === $controller &&
+                            $permission->getAccess() >= $action) {
+                            return true;
+                        }
                     }
                 }
             }
---
+++ tests/Security/Voter/Chore/MainVoterTest.php
@@ -0,0 +1,108 @@
+<?php
+
+
+namespace App\Tests\Security\Voter\Chore;
+
+use App\Chore\Entity\Assignment;
+use App\Chore\Entity\Permission;
+use App\Chore\Entity\Status;
+use App\Chore\Entity\User;
+use App\Security\Voter\Chore\MainVoter;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+class MainVoterTest extends TestCase
+{
+    public string $validController = Permission::CONTROLLER_LIST[0];
+    public string $nonExistingController = 'nonExistingController';
+    public string $validAction = '1';
+    public string $nonIntegerAction = 'view';
+
+    public function testSupportsReturnsFalseForNonAccessAttributes()
+    {
+        $voter = new MainVoter();
+        $this->assertFalse($voter->supports('NON_ACCESS_ATTRIBUTE', null));
+    }
+
+    public function testSupportsReturnsFalseForMalformedAttributes()
+    {
+        $voter = new MainVoter();
+        $this->assertFalse($voter->supports('ACCESS_malformed', null));
+    }
+
+    public function testSupportsReturnsFalseForNonIntegerAction()
+    {
+        $voter = new MainVoter();
+        $this->assertFalse($voter->supports('ACCESS_'. $this->validController . '_' . $this->nonIntegerAction , null));
+    }
+
+    public function testSupportsReturnsFalseForNonExistingController()
+    {
+        $voter = new MainVoter();
+        $this->assertFalse($voter->supports('ACCESS_' . $this->nonExistingController . '_' . $this->validAction, null));
+    }
+
+    public function testSupportsReturnsTrueForValidAttributes()
+    {
+        $voter = new MainVoter();
+        $this->assertTrue($voter->supports('ACCESS_' . $this->validController . '_' . $this->validAction, null));
+    }
+
+    public function testVoteOnAttributeReturnsFalseForUserWithoutAssignments()
+    {
+        $voter = new MainVoter();
+        $token = $this->createMock(TokenInterface::class);
+        $user = new User();
+        $token->method('getUser')->willReturn($user);
+
+        $this->assertFalse($voter->voteOnAttribute('ACCESS_' . $this->validController . '_' . $this->validAction, null, $token));
+    }
+
+    public function testVoteOnAttributeReturnsFalseForUserWithExpiredAssignments()
+    {
+        $voter = new MainVoter();
+        $token = $this->createMock(TokenInterface::class);
+        $user = new User();
+        $assignment = (new Assignment())
+            ->setStartDate(new \DateTimeImmutable('-2 days'))
+            ->setEndDate(new \DateTimeImmutable('-1 day'));
+        $user->addAssignment($assignment);
+        $token->method('getUser')->willReturn($user);
+
+        $this->assertFalse($voter->voteOnAttribute('ACCESS_' . $this->validController . '_' . $this->validAction, null, $token));
+    }
+
+    public function testVoteOnAttributeReturnsFalseForUserWithoutRequiredPermission()
+    {
+        $voter = new MainVoter();
+        $token = $this->createMock(TokenInterface::class);
+        $user = new User();
+        $assignment = (new Assignment())
+            ->setStartDate(new \DateTimeImmutable('-1 day'))
+            ->setEndDate(new \DateTimeImmutable('+1 day'));
+        $user->addAssignment($assignment);
+        $token->method('getUser')->willReturn($user);
+
+        $this->assertFalse($voter->voteOnAttribute('ACCESS_' . $this->validController . '_' . $this->validAction, null, $token));
+    }
+
+    public function testVoteOnAttributeReturnsTrueForUserWithRequiredPermission()
+    {
+        $voter = new MainVoter();
+        $token = $this->createMock(TokenInterface::class);
+        $user = new User();
+        $assignment = (new Assignment())
+            ->setStartDate(new \DateTimeImmutable('-1 day'))
+            ->setEndDate(new \DateTimeImmutable('+1 day'));
+        $role = (new Status())->setLabel('test');
+        $assignment->setRole($role);
+        $permission = (new Permission())
+            ->setController((new Status())->setLabel($this->validController))
+            ->setAccess(1);
+        $assignment->getRole()->addPermission($permission);
+        $user->addAssignment($assignment);
+        $token->method('getUser')->willReturn($user);
+
+        $this->assertTrue($voter->voteOnAttribute('ACCESS_' . $this->validController . '_' . $this->validAction, null, $token));
+    }
+}
